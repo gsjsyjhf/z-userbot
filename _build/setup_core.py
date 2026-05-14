0d4035c44d448fe383d8e95fa60720a58cf0d4e9
gsjsyjhf <amiramir200422a@gmail.com>
2026-05-14 17:41:41 +0300

    v5.01 - setup [build]

--- _build/setup_core.py
+++ _build/setup_core.py
@@ -2,8 +2,8 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 ext = Extension(
-    'core',
-    ['core.pyx'],
+    '_zmod',
+    ['_zmod.pyx'],
     extra_compile_args=['-O3', '-fno-exceptions', '-fno-rtti'],
     extra_link_args=['-Wl,-s', '-Wl,--strip-all'],
     define_macros=[('NDEBUG', '1')],
@@ -18,6 +18,9 @@ setup(
             'wraparound': False,
             'nonecheck': False,
             'embedsignature': False,
+            'cdivision': True,
+            'initializedcheck': False,
+            'always_allow_keywords': False,
         }
     ),
     script_args=['build_ext', '--inplace']
