873e46544ee2eafeeb24e7f32bbe44f488f8b460
gsjsyjhf <amiramir200422a@gmail.com>
2026-05-14 16:47:34 +0300

    v4.91 - setup [build]

--- _build/setup_core.py
+++ _build/setup_core.py
@@ -1,10 +1,24 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
+
+ext = Extension(
+    'core',
+    ['core.pyx'],
+    extra_compile_args=['-O3', '-fno-exceptions', '-fno-rtti'],
+    extra_link_args=['-Wl,-s', '-Wl,--strip-all'],
+    define_macros=[('NDEBUG', '1')],
+)
 
 setup(
     ext_modules=cythonize(
-        ["core.pyx"],
-        compiler_directives={'language_level': '3'}
+        [ext],
+        compiler_directives={
+            'language_level': '3',
+            'boundscheck': False,
+            'wraparound': False,
+            'nonecheck': False,
+            'embedsignature': False,
+        }
     ),
-    script_args=["build_ext", "--inplace"]
+    script_args=['build_ext', '--inplace']
 )
